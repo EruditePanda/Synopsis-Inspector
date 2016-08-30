d2f737470b609805ad8e60a633b294d98d1ef0ef
vade <vade@vade.info>
2016-08-30 10:27:06 -0400

    Transparent collectionview in an attempt to fix the UI bug on resize where our background changes color. Nada.

---
+++ Synopsis Inspector/Synopsis Inspector/TransparentCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  TransparentCollectionView.h
+//  Synopsis Inspector
+//
+//  Created by vade on 8/27/16.
+//  Copyright © 2016 v002. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TransparentCollectionView : NSCollectionView
+
+@end
---
+++ Synopsis Inspector/Synopsis Inspector/TransparentCollectionView.m
@@ -0,0 +1,19 @@
+//
+//  TransparentCollectionView.m
+//  Synopsis Inspector
+//
+//  Created by vade on 8/27/16.
+//  Copyright © 2016 v002. All rights reserved.
+//
+
+#import "TransparentCollectionView.h"
+
+@implementation TransparentCollectionView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
